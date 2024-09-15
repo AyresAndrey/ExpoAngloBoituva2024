db228cae307014d5b0754721e2fc75e1a0a2c7b3
Andrey Ayres <45672988+AyresAndrey@users.noreply.github.com>
2024-09-15 00:19:33 -0300

    Changing LCD library and adding temperature sensor reading

--- ph_sensor.ino
+++ ph_sensor.ino
@@ -1,84 +1,86 @@
-#include <Wire.h>
-#include <LiquidCrystal.h> // Adiciona a biblioteca "LiquidCrystal" ao projeto
+#include <Wire.h>               // Biblioteca utilizada para fazer a comunicação com o I2C
+#include <LiquidCrystal_I2C.h>  // Biblioteca utilizada para fazer a comunicação com o display 16x2 
 
-LiquidCrystal lcd(12, 13, 7, 6, 5, 4); // Pinagem do LCD
+#define col 16     // Serve para definir o numero de colunas do display utilizado
+#define lin  2     // Serve para definir o numero de linhas do display utilizado
+#define ende  0x27 // Serve para definir o endereço do display.
 
-float valor_calibracao = 21.34 + 5.31;   // Fator de calibração
+LiquidCrystal_I2C lcd(ende,col,lin); // Chamada da funcação LiquidCrystal para ser usada com o I2C
 
-int contagem = 0;           // Variável de contagem
-float soma_tensao = 0;      // Variável para soma de tensão
-float media = 0;            // Variável que calcula a media
-float entrada_A0;           // Variável de leitura do pino A0
-float tensao;               // Variável para conversão em tensão
+int contagem = 0;           // Variável de contagem
+float soma_tensao = 0;      // Variável para soma de tensão
+float media_tensao = 0;     // Variável que calcula a media
+float entrada_A0;           // Variável de leitura do pino A0
+float entrada_A1;           // Variável de leitura do pino A1
+float tensao;               // Variável para conversão em tensão
+float temperatura;          // Variável para temperatura
+float soma_temperatura;
+float media_temperatura;
 unsigned long tempo;        // Float tempo
 
 void setup() {
-  lcd.begin(16, 2);             // Inicializa todos os caracteres
-  inicio ();
   Serial.begin(9600);
   delay(2000);
+
+  lcd.init();      // Serve para iniciar a comunicação com o display já conectado
+  lcd.backlight(); // Serve para ligar a luz do display
+  lcd.clear();     // Serve para limpar a tela do display
+
+  lcd.setCursor(0,0); // Coloca o cursor do display na coluna 0 e linha 0 = 1
+  lcd.print("Expo Anglo 2024");
 }
 
 void loop() {
-  soma_tensao = 0;   // Inicia soma_tensão em 0
-  contagem = 0;      // Inicia a contagem em 0
-
+  soma_tensao = 0;      // Inicia soma_tensão em 0
+  soma_temperatura = 0; // Inicia soma_temperatura em 0
+  contagem = 0;         // Inicia a contagem em 0
+  
   while (contagem < 10) {                   // Executa enquanto contagem menor que 10
+    lcd.setCursor(15,1);                    // Mostra indicador de leitura 
+    lcd.print("*");     
+
     tempo = millis();                       // Define o tempo em microssegundos
-    entrada_A0 = analogRead(A0);            // Lê a entrada analógica
-    tensao = (entrada_A0 * 5.0) / 1024.0;   // Converte em tensão, o valor lido
-    soma_tensao = (soma_tensao + tensao);   // Soma a tensão anterior com a atual
-    contagem++;                             // Soma 1 à variável de contagem
-    delay(100);                             // Aguarda para próxima leitura
+    
+    entrada_A0 = analogRead(A0);            // Lê a entrada analógica
+    entrada_A1 = analogRead(A1);            // Lê a entrada analógica
+    
+    tensao = 5 / 1024.0 * entrada_A0;
+    temperatura = entrada_A1 * 1100 / (1024 * 10);
+    
+    soma_tensao = (soma_tensao + tensao);                   // Soma a tensão anterior com a atual
+    soma_temperatura = (soma_temperatura + temperatura);    // Soma a temperatura anterior com a atual
+    contagem++;                                             // Soma 1 a variável de contagem
+    delay(100);                                             // Aguarda para próxima leitura
   }
 
-  media = soma_tensao / 10;                 // Calcula a média das leituras
+  media_tensao = soma_tensao / 10;                    // Calcula a média das leituras
+  media_temperatura = soma_temperatura / 10;
+  
+  float valor_pH = 7 + ((2.5 - media_tensao) / 0.18);           // Calcula valor de pH 
+  float valor_temperaturaC = (media_temperatura - 32) * 5 /9;   // Converte temperatura em fahrenheit para celsius
 
-  float valor_pH = -5.70 * media + valor_calibracao;    // Calcula valor de pH
+  //Saída para o LCD do PH
+  lcd.setCursor(0,1);       // Cursor na linha 2 coluna 0
+  lcd.print("pH:");         // Imprime o texto pH
+  lcd.setCursor(3,1);
+  lcd.print(valor_pH,1);    // Imprime o valor do PH
 
-  lcd.clear();              // Limpa o display
-  lcd.setCursor(2, 0);      // 2 = 2 colunas para a direita. 0 = Primeira linha
-  lcd.print("Valor do pH:"); // Imprime um texto
+  //Saída para o LCD da temperatura
+  lcd.setCursor(8,1);                   // Cursor na linha 2 coluna 8
+  lcd.print("Tp:");                     // Imprime o texto Tp
+  lcd.setCursor(11,1);
+  lcd.print(valor_temperaturaC,1);      // Imprime o valor da temperatura  
 
-  if (valor_pH < 10)
-    lcd.setCursor(6, 1);
-  else
-    lcd.setCursor(5, 1);      // 2 = 2 colunas para a direita. 0 = Primeira linha
-  lcd.print(valor_pH); // Imprime um texto
+  lcd.setCursor(15,1);     // Limpa indicador de leittura
+  lcd.print(" ");   
+
+  //Saída para o Serial
+  Serial.print("Valor PH:");
   Serial.println(valor_pH);
 
-  delay(1000);                    // Aguarda para próxima leitura
-}
+  //Saída para o Serial
+  Serial.print("Temperatura:");
+  Serial.println(valor_temperaturaC);  
 
-void inicio ()                // teste de rolagem de mensagem
-{
-  lcd.setCursor(0, 0);           // selecionando coluna 16 e linha 1
-  lcd.print("M"); 
-  delay (200);
-  lcd.print("e"); 
-  delay (200);
-  lcd.print("d"); 
-  delay (200);
-  lcd.print("i"); 
-  delay (200);
-  lcd.print("d"); 
-  delay (200);
-  lcd.print("o"); 
-  delay (200);
-  lcd.print("r"); 
-  delay (200);
-  lcd.print(" "); 
-  delay (200);
-  lcd.print("d"); 
-  delay (200);
-  lcd.print("e"); 
-  delay (200);
-  lcd.print(" "); 
-  delay (200);
-  lcd.print("p"); 
-  delay (200);
-  lcd.print("H"); 
-  delay (200);
-  lcd.print(":"); 
-  delay (200);
+  delay(1000); // Aguarda para próxima leitura
 }
